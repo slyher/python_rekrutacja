30858d2f688a67d09595331385d811656afda67e
Sylwester Herber <sylwester@herber.pl>
2019-04-09 21:22:35 +0200

    second solution - some optimisation

--- test.py
+++ test.py
@@ -12,5 +12,14 @@ def stage_one(st: str):  # brute force
                 print(st[i - 1], end='')
 
 
+def stage_two(st: str):  # lets think about optimisation
+    for i in range(int(len(st) / 2)):
+        st_temp = st[2 * i: 2 * (i + 1)]
+        for j in range(int(st_temp[1])):
+            print(st_temp[0], end='')
+
+
 stage_one(s)
+print()
+stage_two(s)
 print()
