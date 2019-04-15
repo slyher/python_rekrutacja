2c882faa6d1c1c2124ca931723da83a7c97bc1fd
Sylwester Herber <sylwester@herber.pl>
2019-04-15 20:55:01 +0200

    moved all to class

--- test.py
+++ test.py
@@ -2,54 +2,49 @@
 Work interview for python developer
 convert: a4g2h6 to: aaaagghhhhhh
 """
+
+
+class StringDecoder:
+    def stage_one(self, st: str):  # brute force
+        for i in range(len(st)):
+            if i % 2 == 1:
+                for j in range(int(st[i])):
+                    print(st[i - 1], end='')
+
+    def stage_two(self, st: str):  # lets think about optimisation
+        for i in range(int(len(st) / 2)):
+            st_temp = st[2 * i: 2 * (i + 1)]
+            for j in range(int(st_temp[1])):
+                print(st_temp[0], end='')
+
+    def stage_three(self, st: str):  # more optimisation
+        for i in range(int(len(st) / 2)):
+            for j in range(int(st[2 * i + 1])):
+                print(st[2 * i], end='')
+
+    def stage_four(self, st: str):  # let it lok nice too
+        for i in range(len(st)):
+            if i % 2 == 0:
+                print(int(st[i + 1]) * st[i], end='')
+
+    def stage_five(self, st: str):  # final version
+        for i in range(int(len(st) / 2)):
+            print(int(st[2 * i + 1]) * st[2 * i], end='')
+
+    def stage_six(self, st: str):  # regexp bonus :P
+        import re
+        for st_temp in re.findall(r"\D+\d+", st):
+            print(int(st_temp[1]) * st_temp[0], end='')
+
 s = "a4g2h6"
-
-
-def stage_one(st: str):  # brute force
-    for i in range(len(st)):
-        if i % 2 == 1:
-            for j in range(int(st[i])):
-                print(st[i - 1], end='')
-
-
-def stage_two(st: str):  # lets think about optimisation
-    for i in range(int(len(st) / 2)):
-        st_temp = st[2 * i: 2 * (i + 1)]
-        for j in range(int(st_temp[1])):
-            print(st_temp[0], end='')
-
-
-def stage_three(st: str):  # more optimisation
-    for i in range(int(len(st) / 2)):
-        for j in range(int(st[2 * i + 1])):
-            print(st[2 * i], end='')
-
-
-def stage_four(st: str):  # let it lok nice too
-    for i in range(len(st)):
-        if i % 2 == 0:
-            print(int(st[i + 1]) * st[i], end='')
-
-
-def stage_five(st: str):  # final version
-    for i in range(int(len(st) / 2)):
-        print(int(st[2 * i + 1]) * st[2 * i], end='')
-
-
-def stage_six(st: str):  # regexp bonus :P
-    import re
-    for st_temp in re.findall(r"\D+\d+", st):
-        print(int(st_temp[1]) * st_temp[0], end='')
-
-
-stage_one(s)
+StringDecoder.stage_one(s)
 print()
-stage_two(s)
+StringDecoder.stage_two(s)
 print()
-stage_three(s)
+StringDecoder.stage_three(s)
 print()
-stage_four(s)
+StringDecoder.stage_four(s)
 print()
-stage_five(s)
+StringDecoder.stage_five(s)
 print()
-stage_six(s)
+StringDecoder.stage_six(s)
